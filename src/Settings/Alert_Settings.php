1d5cbaf25a65250dd559335112562a037cce3780
Justin Frydman <justin.frydman@gmail.com>
2022-04-14 16:44:07 -0600

    Add settings

---
+++ src/Settings/Alert_Settings.php
@@ -0,0 +1,23 @@
+<?php declare( strict_types=1 );
+
+namespace Tribe\Alert\Settings;
+
+use Tribe\Alert\Post_Types\Alert\Alert;
+use Tribe\Libs\ACF\ACF_Settings;
+
+class Alert_Settings extends ACF_Settings {
+
+	public function get_title(): string {
+		return __( 'Settings', 'tribe-alerts' );
+	}
+
+	public function get_capability(): string {
+		return 'activate_plugins';
+	}
+
+	public function get_parent_slug(): string {
+		return sprintf( 'edit.php?post_type=%s', Alert::NAME );
+	}
+
+
+}
