2f51361795b582e8fb9ca040782608c9dc7b6c34
Justin Frydman <justin.frydman@gmail.com>
2022-04-14 16:44:35 -0600

    add alert meta/settings meta

---
+++ src/Meta/Meta_Definer.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Tribe\Alert\Meta;
+
+use DI;
+use Psr\Container\ContainerInterface;
+use Tribe\Alert\Post_Types\Alert\Alert;
+use Tribe\Alert\Settings\Alert_Settings;
+use Tribe\Libs\Container\Definer_Interface;
+use Tribe\Libs\Object_Meta\Object_Meta_Definer;
+
+class Meta_Definer implements Definer_Interface {
+
+	public function define(): array {
+		return [
+			Object_Meta_Definer::GROUPS => DI\add( [
+				DI\get( Alert_Settings_Meta::class ),
+				DI\get( Alert_Meta::class ),
+			] ),
+
+			Alert_Settings_Meta::class  => DI\autowire()
+				->constructorParameter( 'object_types', static fn( ContainerInterface $c ) => [
+					'settings_pages' => [ $c->get( Alert_Settings::class )->get_slug() ],
+				] ),
+
+			Alert_Meta::class           => DI\autowire()
+				->constructorParameter( 'object_types', static fn( ContainerInterface $c ) => [
+					'post_types' => [ Alert::NAME ],
+				] ),
+		];
+	}
+
+}
