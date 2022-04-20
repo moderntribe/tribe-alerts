712cfec2312714ff8054807b2d98e9edb12afbd5
Justin Frydman <justin.frydman@gmail.com>
2022-04-20 09:45:32 -0600

    Add closing semi colons

--- resources/views/alert.php
+++ resources/views/alert.php
@@ -10,7 +10,7 @@
 <div
 	class="tribe-alerts"
 	id="tribe-alerts"
-	data-alert-id="<?php echo $this->e( $dto->id ) ?>"
+	data-alert-id="<?php echo $this->e( $dto->id ); ?>"
 >
 	<div class="tribe-alerts__container">
 
@@ -22,18 +22,18 @@
 		</button>
 
 		<?php if ( $dto->title ) : ?>
-			<h2 class="tribe-alerts__title"><?php echo $this->e( $dto->title ) ?></h2>
+			<h2 class="tribe-alerts__title"><?php echo $this->e( $dto->title ); ?></h2>
 		<?php endif; ?>
 
 		<?php if ( $dto->content ) : ?>
-			<div class="tribe-alerts__content"><?php echo $this->e( $dto->content ) ?></div>
+			<div class="tribe-alerts__content"><?php echo $this->e( $dto->content ); ?></div>
 		<?php endif; ?>
 
 		<?php if ( $dto->cta->url ) : ?>
 			<a class="a-link tribe-alerts__link"
-				<?php echo $link_attributes ?>
-			   href="<?php echo $this->e( $dto->cta->url, 'esc_url' ) ?>">
-				<?php echo $this->e( $dto->cta->title ) ?>
+				<?php echo $link_attributes; ?>
+			   href="<?php echo $this->e( $dto->cta->url, 'esc_url' ); ?>">
+				<?php echo $this->e( $dto->cta->title ); ?>
 			</a>
 		<?php endif; ?>
 	</div>
