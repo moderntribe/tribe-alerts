1a59041d2d82767495b490e5fe0450da9a5ec6e4
Justin Frydman <justin.frydman@gmail.com>
2022-04-29 12:42:21 -0600

    Bump to version 1.0.6

--- core.php
+++ core.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Tribe Alerts
  * Plugin URI:        https://github.com/moderntribe/tribe-alerts
  * Description:       Tribe Alerts WordPress Plugin
- * Version:           1.0.0
+ * Version:           1.0.6
  * Requires PHP:      7.4
  * Author:            Modern Tribe
  * Author URI:        https://tri.be
