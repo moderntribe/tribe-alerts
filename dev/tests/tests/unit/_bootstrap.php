1bc009ddcf99f11a257d2aaba70c9af5efd0b266
Mykhailo Los <mlkilderkin@gmail.com>
2026-03-31 17:02:41 +0300

    Change codecept plugins folder path

--- dev/tests/tests/unit/_bootstrap.php
+++ dev/tests/tests/unit/_bootstrap.php
@@ -2,7 +2,7 @@
 
 use tad\FunctionMocker\FunctionMocker;
 
-\Codeception\Util\Autoload::addNamespace( 'Tribe\\Project', __DIR__ . '/../../../../wp-content/plugins/core/src' );
+\Codeception\Util\Autoload::addNamespace( 'Tribe\\Project', __DIR__ . '/../../../public/wp-content/plugins/core/src' );
 
 $function_mocker_cache_dir = realpath( sys_get_temp_dir() ) . DIRECTORY_SEPARATOR . 'function-mocker';
 is_dir( $function_mocker_cache_dir ) || mkdir( $function_mocker_cache_dir );
